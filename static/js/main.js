7810a87711b323a51b261388c83fb227948cee41
jolth <jolthgs@gmail.com>
2014-01-30 17:46:20 -0500

    Class googleMaps

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,45 @@
 
+// Class googleMaps
+var googleMaps = { 
+
+ init : function (lat, lng) { 
+            
+  var styles = [
+   {
+     stylers: [
+       //{ hue: "#ffcc00" },
+       { hue: "#646f76" },
+       { saturation: -20 }
+     ]
+   },{
+     featureType: "road",
+     elementType: "geometry",
+     stylers: [
+       { lightness: 0 },
+       { visibility: "simplified" }
+     ]
+   },{
+     featureType: "road",
+     elementType: "labels",
+     stylers: [
+       { visibility: "off" }
+     ]
+   }
+  ];
+
+  var center = new google.maps.LatLng(lat, lng);
+  var mapOptions = {
+     zoom: 6,
+     center: center,
+     disableDefaultUI: true,
+     mapTypeId: google.maps.MapTypeId.ROADMAP
+  }
+  var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+  map.setOptions({styles: styles});
+
+ }
+
+};
+
+google.maps.event.addDomListener(window, 'load', googleMaps.init(4.3464112753331925, -71.12548828125));
+
